812cd4bd50021f6dc310dfdcd79186c2d153435e
Artflix-Design <137072417+Artflix-Design@users.noreply.github.com>
2024-03-14 00:42:55 +0530

    Update cards.js

--- src/components/cards.js
+++ src/components/cards.js
@@ -13,6 +13,8 @@ import image19 from "../assests/image19.png";
 import image25 from "../assests/image25.jpg";
 import AOS from 'aos';
 import { useEffect } from 'react';
+import whatsapp from "../assests/whatsapp.png";
+import Nav from 'react-bootstrap/Nav';
 function BasicExample() {
   useEffect(() => {
     AOS.init({
@@ -25,23 +27,23 @@ function BasicExample() {
     <Container >
       <h2 className='h2' data-aos="fade-right" > Our Services</h2>
         <Row  data-aos="fade-up" >
-            <Col className='Col' >
+            <Col  lg={4} md={6} sm={12} xs={12}  className='py-4'>
     <Card className='Cards' style={{"borderRadius":"10px"}} >
       <Card.Img variant="top" src={image11} height={200}  alt='cards images'/>
       <Card.Body>
-        <Card.Title className='title'  >Bird Spikes:</Card.Title>
-        <Card.Text className='text'  >
+        <Card.Title className='title'>Bird Spikes</Card.Title>
+        <Card.Text className='text'>
         Bird spike nets are effective bird deterrent systems commonly used on buildings, ledges, and other structures to prevent birds from perching or roosting.
         </Card.Text>
         <Row >
-            <Col><a href='/services'><h6 style={{"font-weight":"600","fontSize":"16px","color":"#0094FF"}}>View more</h6></a></Col>
-       <Col> <Button variant='success' href="tel:+919866678788" style={{"background-color":"#049E79","width":"176px","border-radius":"10px"}}>CONTACT</Button></Col>
+        <Col lg={5} md={5} sm={5} xs={5}><a href='/services'><h6 style={{"font-weight":"600","fontSize":"14px","color":"#0094FF",paddingTop:'15px'}}>View more</h6></a></Col>
+        <Col lg={2} md={2} sm={2} xs={2}> <Nav.Link   href="https://wa.me/+919866678788" style={{"paddingRight":"0px"}} ><img src={whatsapp} style={{"padding":"0px","marginTop":"-8px"}} width={48} alt="whatsapp"></img></Nav.Link></Col>
+       <Col lg={4} md={4} sm={4} xs={4}><center> <Button variant='success' href="tel:+919866678788" style={{"background-color":"#049E79","border-radius":"10px"}}>CONTACT</Button></center></Col>
        </Row>
-
       </Card.Body>
     </Card>
     </Col>
-    <Col className='Col' >
+    <Col  lg={4} md={6} sm={12} xs={12}  className='py-4'>
     <Card className='Cards' style={{"borderRadius":"10px"}}>
       <Card.Img variant="top" src={image18}  height={200}  alt='cards images'/>
       <Card.Body>
@@ -50,14 +52,15 @@ function BasicExample() {
         Pigeon safety nets are durable barriers made of materials like nylon or stainless steel, preventing birds from entering spaces like balconies and windows. 
         </Card.Text>
         <Row >
-            <Col><a href='/services'><h6 style={{"font-weight":"600","fontSize":"16px","color":"#0094FF"}}>View more</h6></a></Col>
-       <Col> <Button variant='success' href="tel:+919866678788" style={{"background-color":"#049E79","width":"176px","border-radius":"10px"}}>CONTACT</Button></Col>
+        <Col lg={5} md={5} sm={5} xs={5}><a href='/services'><h6 style={{"font-weight":"600","fontSize":"14px","color":"#0094FF",paddingTop:'15px'}}>View more</h6></a></Col>
+        <Col lg={2} md={2} sm={2} xs={2}> <Nav.Link   href="https://wa.me/+919866678788" style={{"paddingRight":"0px"}} ><img src={whatsapp} style={{"padding":"0px","marginTop":"-8px"}} width={48} alt="whatsapp"></img></Nav.Link></Col>
+       <Col lg={4} md={4} sm={4} xs={4}><center> <Button variant='success' href="tel:+919866678788" style={{"background-color":"#049E79","border-radius":"10px"}}>CONTACT</Button></center></Col>
        </Row>
 
       </Card.Body>
     </Card>
     </Col>
-    <Col className='Col'>
+    <Col lg={4} md={6} sm={12} xs={12}  className='py-4'>
     <Card className='Cards' style={{"borderRadius":"10px"}}>
       <Card.Img variant="top" src={image17}  height={200}  alt='cards images'/>
       <Card.Body>
@@ -66,15 +69,14 @@ function BasicExample() {
         A children's safety net is a holistic system of support and protection aimed at ensuring the safety, well-being and development of children.
         </Card.Text>
         <Row >
-            <Col><a href='/services'><h6 style={{"font-weight":"600","fontSize":"16px","color":"#0094FF"}}>View more</h6></a></Col>
-       <Col> <Button  variant='success' href="tel:+919866678788" style={{"background-color":"#049E79","width":"176px","border-radius":"10px"}}>CONTACT</Button></Col>
+        <Col lg={5} md={5} sm={5} xs={5}><a href='/services'><h6 style={{"font-weight":"600","fontSize":"14px","color":"#0094FF",paddingTop:'15px'}}>View more</h6></a></Col>
+        <Col lg={2} md={2} sm={2} xs={2}> <Nav.Link   href="https://wa.me/+919866678788" style={{"paddingRight":"0px"}} ><img src={whatsapp} style={{"padding":"0px","marginTop":"-8px"}} width={48} alt="whatsapp"></img></Nav.Link></Col>
+       <Col lg={4} md={4} sm={4} xs={4}><center> <Button variant='success' href="tel:+919866678788" style={{"background-color":"#049E79","border-radius":"10px"}}>CONTACT</Button></center></Col>
        </Row>
       </Card.Body>
     </Card>
     </Col>
-    </Row>
-    <Row className='Row'  data-aos="fade-up">
-            <Col className='Col'>
+            <Col  lg={4} md={6} sm={12} xs={12} className='py-4'>
     <Card className='Cards' style={{"borderRadius":"10px"}}>
       <Card.Img variant="top"  src={image19} height={200}  alt='cards images'/>
       <Card.Body>
@@ -83,42 +85,43 @@ function BasicExample() {
         Anti-bird nets are specialized netting systems designed to prevent birds from roosting, nesting, or entering specific areas. 
         </Card.Text>
         <Row >
-            <Col><a href='/services'><h6 style={{"font-weight":"600","fontSize":"16px","color":"#0094FF"}}>View more</h6></a></Col>
-       <Col> <Button variant='success' href="tel:+919866678788" style={{"background-color":"#049E79","width":"176px","border-radius":"10px"}}>CONTACT</Button></Col>
+        <Col lg={5} md={5} sm={5} xs={5}><a href='/services'><h6 style={{"font-weight":"600","fontSize":"14px","color":"#0094FF",paddingTop:'15px'}}>View more</h6></a></Col>
+        <Col lg={2} md={2} sm={2} xs={2}> <Nav.Link   href="https://wa.me/+919866678788" style={{"paddingRight":"0px"}} ><img src={whatsapp} style={{"padding":"0px","marginTop":"-8px"}} width={48} alt="whatsapp"></img></Nav.Link></Col>
+       <Col lg={4} md={4} sm={4} xs={4}><center> <Button variant='success' href="tel:+919866678788" style={{"background-color":"#049E79","border-radius":"10px"}}>CONTACT</Button></center></Col>
        </Row>
-
       </Card.Body>
     </Card>
     </Col>
-    <Col className='Col'>
+    <Col lg={4} md={6} sm={12} xs={12}  className='py-4'>
     <Card className='Cards' style={{"borderRadius":"10px"}}>
-      <Card.Img variant="top"  src={image38}height={200}   alt='cards images'/>
+      <Card.Img variant="top"  src={image38}height={200}  alt='cards images'/>
       <Card.Body>
         <Card.Title  className='title' >Duct Area Safety Nets</Card.Title>
         <Card.Text className='text' >
         Duct area safety nets are essential safety measures for workers in confined or elevated spaces like ducts and tunnels. 
         </Card.Text>
         <Row >
-            <Col><a href='/services'><h6 style={{"font-weight":"600","fontSize":"16px","color":"#0094FF"}}>View more</h6></a></Col>
-       <Col> <Button variant='success' href="tel:+919866678788" style={{"background-color":"#049E79","width":"176px","border-radius":"10px"}}>CONTACT</Button></Col>
+        <Col lg={5} md={5} sm={5} xs={5}><a href='/services'><h6 style={{"font-weight":"600","fontSize":"14px","color":"#0094FF",paddingTop:'15px'}}>View more</h6></a></Col>
+        <Col lg={2} md={2} sm={2} xs={2}> <Nav.Link   href="https://wa.me/+919866678788" style={{"paddingRight":"0px"}} ><img src={whatsapp} style={{"padding":"0px","marginTop":"-8px"}} width={48} alt="whatsapp"></img></Nav.Link></Col>
+       <Col lg={4} md={4} sm={4} xs={4}><center> <Button variant='success' href="tel:+919866678788" style={{"background-color":"#049E79","border-radius":"10px"}}>CONTACT</Button></center></Col>
        </Row>
 
       </Card.Body>
     </Card>
     </Col>
-    <Col className='Col'>
+    <Col  lg={4} md={6} sm={12} xs={12}  className='py-4'>
     <Card className='Cards' style={{"borderRadius":"10px"}}>
-      <Card.Img variant="top" src={image25} alt='cards images'/>
+      <Card.Img variant="top" src={image25}  height={200} alt='cards images'/>
       <Card.Body>
         <Card.Title  className='title' >Construction Safety Nets:</Card.Title>
         <Card.Text className='text' >
         Construction safety nets are vital safety devices used on construction sites to prevent accidents and protect workers.
         </Card.Text>
-        <Row >
-            <Col><a href='VBEnterprise/services'><h6 style={{"font-weight":"600","fontSize":"16px","color":"#0094FF"}}>View more</h6></a></Col>
-       <Col> <Button variant='success' href="tel:+919866678788" style={{"background-color":"#049E79","width":"176px","border-radius":"10px"}}>CONTACT</Button></Col>
+          <Row >
+        <Col lg={5} md={5} sm={5} xs={5}><a href='/services'><h6 style={{"font-weight":"600","fontSize":"14px","color":"#0094FF",paddingTop:'15px'}}>View more</h6></a></Col>
+        <Col lg={2} md={2} sm={2} xs={2}> <Nav.Link   href="https://wa.me/+919866678788" style={{"paddingRight":"0px"}} ><img src={whatsapp} style={{"padding":"0px","marginTop":"-8px"}} width={48} alt="whatsapp"></img></Nav.Link></Col>
+       <Col lg={4} md={4} sm={4} xs={4}><center> <Button variant='success' href="tel:+919866678788" style={{"background-color":"#049E79","border-radius":"10px"}}>CONTACT</Button></center></Col>
        </Row>
-
       </Card.Body>
     </Card>
     </Col>

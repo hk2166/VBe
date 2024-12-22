b3a195d69e5f80f6cc3a1fcca73fcaa7787713e8
Artflix-Design <137072417+Artflix-Design@users.noreply.github.com>
2024-12-22 15:16:30 +0530

    Add files via upload

---
+++ src/components/Home.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import './home.scss'
+import Carousel from "../components/Carousel";
+
+function Home(){
+    return(
+    <>
+           <div className="home_background"> 
+        <Carousel/>
+  
+  </div>
+    </>
+    )
+}
+export default Home;

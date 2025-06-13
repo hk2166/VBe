7d53c781387bc882f9eb326e1b7af21385638ca8
hk2166 <9610hemant@gmail.com>
2025-06-13 11:55:51 +0530

    Initial commit: Adding VBEnterprise website code

--- src/pages/services.js
+++ src/pages/services.js
@@ -1,18 +1,19 @@
 import  { lazy, Suspense } from 'react';
 const LazyComponent7 = lazy(() => import('../components/content.js'));
 const LazyComponent8 = lazy(() => import('../components/front-image-services.js'));
+const LazyComponent13 = lazy(() => import('../components/cards.js'));
+
 function ServicePage() {
    
     return (
         <>
          <Suspense fallback={<div>Loading...</div>}>
- 
- <LazyComponent8  />
-      <LazyComponent7 />
-    
- </Suspense>
-      
-       </>
+            <LazyComponent8 />
+            <LazyComponent13 />
+            <LazyComponent7 />
+            
+         </Suspense>
+        </>
     )
 }
 export default ServicePage;
